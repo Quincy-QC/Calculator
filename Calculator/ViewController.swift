5e358273bee6163df3628c265feea626af80cd78
UntilYou-QC <578184833@qq.com>
2016-07-29 10:37:03 +0800

    Calculator
    To be continuted

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var display: UILabel!
+
+    var userIsClickingDigit: Bool = false
+    
+    @IBAction func clickDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsClickingDigit {
+            display.text = display.text! + digit  // + 字符串拼接
+        } else {
+            display.text = digit
+            if display.text != "0" {
+                userIsClickingDigit = true
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var operantStack = Array<Double>() 
+    
+    @IBAction func enter() {
+        userIsClickingDigit = false
+        operantStack.append(displayValue)
+        print("operantStack = \(operantStack)")
     }
-
-
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text =  "\(newValue)"
+            userIsClickingDigit = false
+        }
+    }
+    
 }
 
